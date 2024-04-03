b7a7babf5d450caa190219e9600bde55e1da618c
전윤철 <138593109+mosy2266@users.noreply.github.com>
2024-04-03 22:17:46 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,48 +1,31 @@
 import sys
 input=sys.stdin.readline
 
-class stack:
-    def __init__(self):
-        self.items=[]
-    
-    def push(self, item):
-        self.items.append(item)
+N=int(input())
+stack=[]
 
-    def pop(self):
-        if self.items:
-            print(self.items.pop())
+for _ in range(N):
+    prcs=input().rstrip()
+    if "push" in prcs:
+        stack.append(int(prcs[5:]))
+    elif prcs=="pop":
+        if stack:
+            print(stack.pop())
         else:
             print(-1)
-    
-    def size(self):
-        print(len(self.items))
-    
-    def empty(self):
-        if self.items:
+    elif prcs=="size":
+        print(len(stack))
+    elif prcs=="empty":
+        if stack:
             print(0)
         else:
             print(1)
-    
-    def top(self):
-        if self.items:
-            print(self.items[-1])
+    elif prcs=="top":
+        if stack:
+            print(stack[-1])
         else:
             print(-1)
 
-N=int(input())
-ex=stack()
-
-for _ in range(N):
-    prcs=input()
-    if "push" in prcs:
-        num=int(prcs[5:])
-        ex.push(num)
-    else:
-        if "pop" in prcs:
-            ex.pop()
-        elif "size" in prcs:
-            ex.size()
-        elif "empty" in prcs:
-            ex.empty()
-        elif "top" in prcs:
-            ex.top()
+'''prcs=="pop" << 이렇게 조건식을 쓰려면
+.rstrip()(맨 오른쪽 문자 제거)을 사용해서 개행문자를 제거해줘야 함
+왜냐면 sys.stdin.readline은 한 줄 단위로 입력받기 때문에 \n도 함께 들어오기 때문,,'''
