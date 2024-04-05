ad54e553bc160e024cadde5c1921fc477c0282cd
전윤철 <138593109+mosy2266@users.noreply.github.com>
2024-04-05 16:57:34 +0900

    Update 괄호.py

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -5,12 +5,12 @@ N=int(input())
 
 for _ in range (N):
     PS=input().rstrip()
-    if PS[0]=="(":
-        while "()" in PS:
-            PS=PS.replace("()","")
-        if not PS:
-            print("YES")
-        else:
-            print("NO")
+    while "()" in PS:
+        PS=PS.replace("()","")
+    if not PS:
+        print("YES")
     else:
         print("NO")
+
+#모든 VPS는 "()"로 이루어져 있음을 이용해 풀이
+#.remove() 메서드는 문자열에서 사용 불가 -> .replace() 메서드를 사용
