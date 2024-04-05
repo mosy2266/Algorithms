e5295dc20e254221f7ca78fe8893dd73bd0f16cb
전윤철 <138593109+mosy2266@users.noreply.github.com>
2024-04-05 17:11:33 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+
+while True:
+    A,B,C=map(int, input().split())
+    if A==0 and B==0 and C==0:
+        break;
+    else:
+        if A**2+B**2==C**2 or A**2+C**2==B**2 or B**2+C**2==A**2:
+            print("right")
+        else:
+            print("wrong")
