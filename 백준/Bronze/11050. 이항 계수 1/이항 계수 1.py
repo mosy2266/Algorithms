19fb3765834b56f1545ec4b2e01ae7b55d6805fc
전윤철 <138593109+mosy2266@users.noreply.github.com>
2024-04-06 20:10:37 +0900

    [Bronze I] Title: 이항 계수 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,4 +1,10 @@
-from math import factorial
-
 N,K=map(int, input().split())
-print(int(factorial(N)/(factorial(K)*factorial(N-K))))
+a=b=1
+#분자의 경우 N!/(N-K)! = (N * N-1 * ... * N-K+1)가 되므로
+for i in range(N, N-K, -1):
+    a*=i
+#분모의 경우 K!만 남으므로
+for i in range(1,K+1):
+    b*=i
+#정수형으로 출력해야 함
+print(int(a/b))
