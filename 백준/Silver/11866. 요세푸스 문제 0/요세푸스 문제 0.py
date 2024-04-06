1d56271439434a885fc70e39febb538126cb3472
전윤철 <138593109+mosy2266@users.noreply.github.com>
2024-04-06 19:51:36 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 64 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -2,11 +2,10 @@ from collections import deque
 
 N,K=map(int, input().split())
 nums=deque([i for i in range(1,N+1)])
+answer=[]
 
-print("<", end="")
 while nums:
     nums.rotate(-(K-1))
-    print(nums.popleft(), end="")
-    if len(nums)!=0:
-        print(", ", end="")
-print(">")
+    answer.append(str(nums.popleft()))
+
+print("<",", ".join(answer),">", sep="")    
