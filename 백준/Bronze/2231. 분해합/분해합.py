3cf7f6041f9391dbd3c9c80b6d05d849a4277a71
전윤철 <138593109+mosy2266@users.noreply.github.com>
2024-04-07 20:50:38 +0900

    [Bronze II] Title: 분해합, Time: 2432 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,18 @@
+N=int(input())
+
+min_num=0
+for i in range(2, N):
+    div_sum=i
+    num=i
+    k=6
+    while k>=1:
+        a=num//(10**k)
+        div_sum+=a
+        num-=a*(10**k)
+        k-=1
+    div_sum+=num
+    if div_sum==N:
+        min_num=i
+        break;
+
+print(min_num)
