a4b116e9538507951b93a782cf07f55b486c423f
전윤철 <138593109+mosy2266@users.noreply.github.com>
2024-04-07 21:04:10 +0900

    [Bronze II] Title: 분해합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,16 +1,9 @@
 N=int(input())
-
 min_num=0
+#각 자리를 더한 값의 최대는 항상 자릿수*9이므로
 for i in range(max(0, N-len(str(N))*9), N):
-    div_sum=i
-    num=i
-    k=6
-    while k>=1:
-        a=num//(10**k)
-        div_sum+=a
-        num-=a*(10**k)
-        k-=1
-    div_sum+=num
+    #map 함수를 이용해서 더해줌
+    div_sum=i+sum(map(int,str(i)))
     if div_sum==N:
         min_num=i
         break;
