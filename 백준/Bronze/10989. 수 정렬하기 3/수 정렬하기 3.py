b1d52ab5fc4beb7de672207edd4dd269d5873d3f
전윤철 <138593109+mosy2266@users.noreply.github.com>
2024-04-11 21:33:04 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8784 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -2,14 +2,13 @@ import sys
 input=sys.stdin.readline
 
 N=int(input())
+#가능한 수의 범위가 1~10000이므로 10001의 크기를 가지도록 선언(인덱스 매칭 편하게)
 cnt_nums=[0]*10001
 
-for _ in range(N):
-    num=int(input())
-    cnt_nums[num]+=1
+for _ in range(N): #수를 입력 받으면서 해당 수를 인덱스로 하는 cnt_nums의 값을 1씩 증가
+    cnt_nums[int(input())]+=1
 
 for i in range(1, len(cnt_nums)):
-    if cnt_nums[i]==0:
-        continue
-    for _ in range(cnt_nums[i]):
-        print(i)
+    if cnt_nums[i]!=0: #개수가 0이라면 출력 X
+        for _ in range(cnt_nums[i]):
+            print(i)
