1fb5e822f198ddeccf2b60ab90c9b08c03347aa6
전윤철 <138593109+mosy2266@users.noreply.github.com>
2024-07-05 21:50:15 +0900

    [Bronze IV] Title: 모음의 개수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.py
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -6,9 +6,8 @@ while True:
     cnt=0
     if s=='#':
         break
-    s=list(s)
+    s=list(s.lower())
     for x in s:
-        if (x=='a' or x=='A' or x=='e' or x=='E' or x=='i' or x=='I' or
-        x=='o' or x=='O' or x=='u' or x=='U'):
+        if x=='a' or x=='e' or x=='i' or x=='o' or x=='u':
             cnt+=1
     print(cnt)
