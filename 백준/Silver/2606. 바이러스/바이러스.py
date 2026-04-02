84ad573b7948126c328193d75e6c540092c38c6d
Yun <138593109+mosy2266@users.noreply.github.com>
2026-04-02 21:03:49 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,35 +1,26 @@
 import sys
-from collections import deque
-
 input=sys.stdin.readline
 
 n=int(input())
 m=int(input())
-adj=[[] for i in range(n)]
-visited=[False]*n
-queue=deque([])
+ans=0
 
-for i in range(m):
-    u,v=map(int, input().split())
+adj = [[] for _ in range(n)]
+visited = [False] * n
+
+for _ in range(m):
+    u,v = map(int, input().split())
 
     adj[u-1].append(v)
     adj[v-1].append(u)
 
-def bfs(graph, start, visited, queue):
-    ans=0
+def dfs(graph, start, visited):
+    global ans
     visited[start-1] = True
+    for v in graph[start-1]:
+        if visited[v-1] == False:
+            ans+=1
+            dfs(graph, v, visited)
 
-    for i in graph[start-1]:
-        queue.append(i)
-
-    while queue:
-        u = queue.popleft()
-        visited[u-1] = True
-        ans+=1
-        for v in graph[u-1]:
-            if visited[v-1] == False and v not in queue:
-                queue.append(v)
-
-    return ans
-
-print(bfs(adj, 1, visited, queue))
+dfs(adj, 1, visited)
+print(ans)
