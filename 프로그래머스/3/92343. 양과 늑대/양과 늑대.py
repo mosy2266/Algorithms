700e2a8f604a2243c3d8ee022b9f8a2e20dfadaa
Yun <138593109+mosy2266@users.noreply.github.com>
2026-06-04 15:52:44 +0900

    [level 3] Title: 양과 늑대, Time: 4.07 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,24 +1,21 @@
-import sys
-sys.setrecursionlimit(10**6)
+
 
 def solution(info, edges):
-    graph=[[] for _ in range(len(info))]
-    visited=[False]*len(info)
+    answer = 0
+    graph = [[] for _ in range (len(info))]
     
     for edge in edges:
         graph[edge[0]].append(edge[1])
-        
-    answer = 0
+    
     def dfs(cur, sheep, wolf, nodes):
         nonlocal answer
-        sheep+=info[cur]^1
-        wolf+=info[cur]
+        
+        sheep += info[cur]^1
+        wolf += info[cur]
+        answer = max(answer, sheep)
         
         next_nodes = nodes + graph[cur]
-        
         if cur in next_nodes: next_nodes.remove(cur)
-        
-        answer=max(answer, sheep)
         
         if wolf>=sheep:
             return
