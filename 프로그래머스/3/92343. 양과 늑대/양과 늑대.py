e84d20d6b436b3100358e0e95bda61674a0467b4
Yun <138593109+mosy2266@users.noreply.github.com>
2026-06-09 17:48:42 +0900

    [level 3] Title: 양과 늑대, Time: 1.04 ms, Memory: 12.4 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,27 +1,28 @@
-
+from collections import deque
 
 def solution(info, edges):
-    answer = 0
-    graph = [[] for _ in range (len(info))]
+    answer = 1
+    visited = [False] * (1<<17)
+    visited[(1<<0)] = True
     
-    for edge in edges:
-        graph[edge[0]].append(edge[1])
+    q=deque()
+    q.append((1<<0))
     
-    def dfs(cur, sheep, wolf, nodes):
-        nonlocal answer
-        
-        sheep += info[cur]^1
-        wolf += info[cur]
-        answer = max(answer, sheep)
-        
-        next_nodes = nodes + graph[cur]
-        if cur in next_nodes: next_nodes.remove(cur)
-        
-        if wolf>=sheep:
-            return
-        
-        for n in next_nodes:
-            dfs(n, sheep, wolf, next_nodes[:])
+    while q:
+        cur_node = q.popleft()
+        for p,c in edges:
+            next_nodes = (cur_node | (1<<c))
+            if (cur_node & (1<<p)) and not (cur_node & (1<<c)) and not visited[next_nodes]:
+                wolf, sheep = 0, 0
+                
+                for node in range(len(info)):
+                    if (next_nodes & (1<<node)) == 0 : continue
+                    sheep += info[node] ^ 1
+                    wolf += info[node]
+                    
+                if wolf < sheep:
+                    answer = max(answer, sheep)
+                    visited[next_nodes] = True
+                    q.append(next_nodes)
     
-    dfs(0,0,0,[])
     return answer
