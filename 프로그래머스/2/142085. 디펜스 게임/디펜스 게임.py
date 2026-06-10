1d76b836b49fc07858d9715ddaf7066c17fbbe24
Yun <138593109+mosy2266@users.noreply.github.com>
2026-06-10 14:22:39 +0900

    [level 2] Title: 디펜스 게임, Time: 2913.27 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -1,29 +1,24 @@
 import math
 
-def solution(n, k, enemy):    
-    def check(target, k, enemy):
-        total_enemy = sum(enemy[k:])
-        return True if target>=total_enemy else False
+def solution(n, k, enemy):
+    def check(target, k, mid, enemies):
+        total_enemies = sum(enemies[k:mid])
+        return True if target>=total_enemies else False
     
     def param_search(target, k, enemy):
-        l=1
-        r=len(enemy)
-        max_round=-math.inf
+        l = 1
+        r = len(enemy)
+        maxRound = -math.inf
         
         while l<=r:
-            mid=(l+r)//2
-            cur_round = sorted(enemy[:mid], reverse=True)
-            if check(target, k, cur_round):
-                l=mid+1
-                max_round=mid
+            mid = (l+r)//2
+            enemies = sorted(enemy[:mid], reverse=True)
+            if check(target, k, mid, enemies):
+                l = mid+1
+                maxRound = mid
             else:
-                r=mid-1
-        
-        return max_round
+                r = mid-1
+        return maxRound
     
-    total_round=len(enemy)
-    if k>=total_round: return total_round
-    
-    answer=param_search(n, k, enemy)
-    
+    answer = param_search(n,k,enemy)
     return answer
