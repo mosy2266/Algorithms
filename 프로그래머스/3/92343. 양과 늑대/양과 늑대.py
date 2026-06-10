40df5a6dc351e33e059c6523fe74f8798c788197
Yun <138593109+mosy2266@users.noreply.github.com>
2026-06-10 14:12:38 +0900

    [level 3] Title: 양과 늑대, Time: 0.81 ms, Memory: 12.4 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -5,24 +5,26 @@ def solution(info, edges):
     visited = [False] * (1<<17)
     visited[(1<<0)] = True
     
-    q=deque()
+    q = deque()
     q.append((1<<0))
     
     while q:
-        cur_node = q.popleft()
+        cur_state = q.popleft()
+        
         for p,c in edges:
-            next_nodes = (cur_node | (1<<c))
-            if (cur_node & (1<<p)) and not (cur_node & (1<<c)) and not visited[next_nodes]:
-                wolf, sheep = 0, 0
+            next_state = cur_state | (1<<c)
+            if (cur_state & (1<<p)) and not (cur_state & (1<<c)) and not visited[next_state]:
+                sheep, wolf = 0, 0
                 
                 for node in range(len(info)):
-                    if (next_nodes & (1<<node)) == 0 : continue
+                    if not(next_state & (1<<node)): continue
+                    
                     sheep += info[node] ^ 1
                     wolf += info[node]
-                    
+                
                 if wolf < sheep:
                     answer = max(answer, sheep)
-                    visited[next_nodes] = True
-                    q.append(next_nodes)
+                    q.append(next_state)
+                    visited[next_state]=True
     
     return answer
