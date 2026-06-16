a66fb5a331d47fcf3bada4eb94fe1c5299d2c376
Yun <138593109+mosy2266@users.noreply.github.com>
2026-06-17 01:00:07 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2059.48 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,15 +1,19 @@
-from collections import deque
-
 def solution(elements):
+    nums_set=set()
     n = len(elements)
-    q = deque(elements)
-    sums = set()
     
-    for i in range(1,n+1):
-        for j in range(n):
-            num = sum(list(q)[:i])
-            q.rotate(-1)
-            sums.add(num)
-            
-    answer = len(sums)
-    return answer
+    for i in range(1, n+1):
+        l=0
+        r=i-1
+        
+        for j in range(i):
+            num_sum = sum(elements[:i])
+        nums_set.add(num_sum)
+        
+        for k in range(1,n):
+            r = (r+1)%n
+            num_sum = num_sum - elements[l] + elements[r]
+            l = (l+1)%n
+            nums_set.add(num_sum)
+    
+    return len(nums_set)
