168129a4e06fb5396454308f1d2bb62fb5da7bc2
Yun <138593109+mosy2266@users.noreply.github.com>
2026-06-29 10:45:48 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,39 +1,40 @@
 def solution(n, costs):
     answer = 0
-    costs.sort(key = lambda x: x[2])
+    
     link = [i for i in range(n)]
     sz = [1]*n
-        
+    
     def find(x):
-        if link[x]==x: return x
+        if link[x]==x:
+            return x
         else:
             link[x] = find(link[x])
             return link[x]
     
-    def same(a,b):
-        return find(a) == find(b)
+    def same(x,y):
+        return find(x) == find(y)
     
-    def unite(a,b):
-        a,b = find(a), find(b)
-        if a==b: return
+    def unite(x,y):
+        x,y = find(x), find(y)
+        if x == y: return
         
-        if sz[a] < sz[b]:
-            a,b=b,a
-        sz[a] += sz[b]
-        sz[b] = 0
-        link[b] = a
+        if sz[x]<sz[y]:
+            x,y = y,x
+        sz[x]+=sz[y]
+        sz[y]=0
+        link[y]=x
     
-    edges=0
-    for cost in costs:
-        x,y,w = cost
+    edges = 0
+    costs.sort(key= lambda x: x[2])
+    for a,b,w in costs:
         
-        if same(x,y):
-            continue
+        if same(a,b): continue
+        
         answer+=w
-        unite(x,y)
+        unite(a,b)
         edges+=1
         
         if edges == n-1:
             break
-            
+    
     return answer
