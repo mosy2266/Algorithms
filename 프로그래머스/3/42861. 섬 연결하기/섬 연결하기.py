88c3deb09c48871fc55bf84befbbc5a13bd31023
Yun <138593109+mosy2266@users.noreply.github.com>
2026-07-02 17:04:00 +0900

    [level 3] Title: 섬 연결하기, Time: 0.04 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,6 +1,5 @@
 def solution(n, costs):
     answer = 0
-    
     link = [i for i in range(n)]
     sz = [1]*n
     
@@ -16,25 +15,25 @@ def solution(n, costs):
     
     def unite(x,y):
         x,y = find(x), find(y)
-        if x == y: return
+        if x==y : return
         
         if sz[x]<sz[y]:
             x,y = y,x
+        
         sz[x]+=sz[y]
         sz[y]=0
         link[y]=x
     
     edges = 0
     costs.sort(key= lambda x: x[2])
+    
     for a,b,w in costs:
+        if same(a,b) : continue
         
-        if same(a,b): continue
-        
-        answer+=w
         unite(a,b)
+        answer+=w
         edges+=1
         
-        if edges == n-1:
-            break
-    
+        if edges == n-1: break
+        
     return answer
