32ca35988c9990aecc98c2aefdc3dcc3f8907394
Yun <138593109+mosy2266@users.noreply.github.com>
2026-07-02 16:40:10 +0900

    [level 3] Title: 합승 택시 요금, Time: 67.46 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,12 +1,9 @@
-import heapq, math
+import math, heapq
 
 def solution(n, s, a, b, fares):
     INF = math.inf
     graph = [[] for _ in range(n+1)]
-    
-    points = [a,b,s]
     charge = [0]*(n+1)
-    
     
     for x,y,w in fares:
         graph[x].append((y,w))
@@ -14,24 +11,23 @@ def solution(n, s, a, b, fares):
     
     def dijk(start):
         q = []
-        heapq.heappush(q, (0, start))
+        heapq.heappush(q, (start, 0))
         distance[start] = 0
         
         while q:
-            dist, now = heapq.heappop(q)
-            if distance[now]<dist: continue
-            
-            for i in graph[now]:
-                cost = dist + i[1]
-                if distance[i[0]] > cost:
-                    distance[i[0]] = cost
-                    heapq.heappush(q, (cost, i[0]))
+            now, dist = heapq.heappop(q)
+            if dist > distance[now]: continue
+            for v in graph[now]:
+                cost = dist + v[1]
+                if distance[v[0]] > cost:
+                    distance[v[0]] = cost
+                    heapq.heappush(q, (v[0], cost))
     
-    for p in points:
+    for i in [s,a,b]:
         distance = [INF]*(n+1)
-        dijk(p)
+        dijk(i)
         
-        for i in range(1,n+1):
-            charge[i] += distance[i]
+        for j in range(1, n+1):
+            charge[j] += distance[j]
     
     return min(charge[1:])
